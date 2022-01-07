678f917c0e6d948b75b75358daa707b6562555c4
joshua360x <jkw5499@psu.edu>
2022-01-06 20:31:52 -0500

    rednerItem function + window on load event

--- shop/shop.js
+++ shop/shop.js
@@ -1,6 +1,40 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { checkAuth, getShoppingList, logout, newItem } from '../fetch-utils.js';
+import { renderItem } from '../render-utils.js';
+
+const formEl = document.getElementById('shoppingList-form');
+
+const shoppingListEL = document.querySelector('.shopping-Lists');
 
 
+formEl.addEventListener('submit', async(e) => {
+    e.preventDefault();
+    const data = new FormData(formEl);
+    const quantity = data.get('quantity');
+    const item = data.get('list');
+
+    await newItem(quantity, item);
+    displayShoppingListItems();
+    formEl.reset();
+
+});
+
+window.addEventListener('load', async() => {
+    // const items = await getShoppingList()
+    displayShoppingListItems();
+
+
+});
+
+
+async function displayShoppingListItems() {
+    const items = await getShoppingList();
+
+    shoppingListEL.textContent = '';
+    for (const item of items) {
+        const newItem = renderItem(item);
+        shoppingListEL.append(newItem);
+    }
+}
 
 checkAuth();
 
